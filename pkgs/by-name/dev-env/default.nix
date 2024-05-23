5fe9698963ffdff48d1d8ed90ee14ae19b291ba4
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-23 14:25:44 -0400

    added dev-env

---
+++ pkgs/by-name/dev-env/default.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}:
+
+pkgs.stdenv.mkDerivation {
+  pname = "dev-env";
+  version = "1.0.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "Open-Systems-Innovation";
+    repo = "dev-env";
+    rev = "main";
+    sha256 = "";
+  };
+
+  nativeBuildInputs = [ pkgs.python3 pkgs.direnv ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp create_flake.py $out/bin/dev-env
+    chmod +x $out/bin/dev-env
+  '';
+}
