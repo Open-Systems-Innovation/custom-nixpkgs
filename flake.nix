c7463d06b01949d6de447bd8895994faf2cc6ac6
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-27 14:42:25 -0400

    Improved README and switched it to an org mode file

--- flake.nix
+++ flake.nix
@@ -16,10 +16,14 @@
     {
       overlays.default = final: prev: {
         hello-nix = prev.callPackage ./pkgs/by-name/hello-nix/package.nix { }; 
+        firedrake = prev.callPackage ./pkgs/by-name/firedrake { }; 
+        dev-env = prev.callPackage ./pkgs/by-name/dev-env/package.nix { }; 
       };
 
       packages.${system} = {
         hello-nix = pkgs.callPackage ./pkgs/by-name/hello-nix/package.nix { }; 
+        firedrake = pkgs.callPackage ./pkgs/by-name/firedrake { };
+        dev-env = pkgs.callPackage ./pkgs/by-name/dev-env/package.nix { };
       };
 
     };
