91c2e973448997dc4da603834ad179093d4c4406
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-28 18:00:52 -0400

    canged to cat EOF instead of echo

--- pkgs/by-name/dev-env/package.nix
+++ pkgs/by-name/dev-env/package.nix
@@ -5,6 +5,8 @@ let
   flakeNixContent = builtins.readFile ./dev_flake.nix;
 in
 pkgs.writeShellScriptBin "dev-env" ''
-  echo "${flakeNixContent}" > flake.nix
+  cat <<EOF > flake.nix
+    ${flakeNixContent}
+  EOF
   echo "use flake" > .envrc
 ''
