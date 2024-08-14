44f80d07740250429bd7e954034f826525886ed1
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-08-14 19:55:54 -0400

    fixed another typo "main" vs "make"

--- pkgs/petsc-project/package.nix
+++ pkgs/petsc-project/package.nix
@@ -5,7 +5,7 @@ let
   flakeNixContent = builtins.readFile ./petsc_flake.nix;
   envrcContent = builtins.readFile ./petsc_envrc;
   makefileContent = builtins.readFile ./petsc_makefile;
-  mainContent = builtins.readFile ./petsc_make.c;
+  mainContent = builtins.readFile ./petsc_main.c;
 in
 pkgs.writeShellScriptBin "dev-env" ''
   cat <<'EOF' > flake.nix
