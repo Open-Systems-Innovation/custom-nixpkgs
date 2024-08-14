99acf1d7e9b7c8ce44ae431569edd27ba2ca370c
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-08-14 19:45:19 -0400

    added main.c to petsc-project

--- pkgs/petsc-project/package.nix
+++ pkgs/petsc-project/package.nix
@@ -5,6 +5,7 @@ let
   flakeNixContent = builtins.readFile ./dev_flake.nix;
   envrcContent = builtins.readFile ./dev_envrc;
   makefileContent = builtins.readFile ./petsc_makefile;
+  mainContent = builtins.readFile ./petsc_make.c;
 in
 pkgs.writeShellScriptBin "dev-env" ''
   cat <<'EOF' > flake.nix
@@ -15,6 +16,9 @@ pkgs.writeShellScriptBin "dev-env" ''
   EOF
   cat <<'EOF' > makefile
     ${makefileContent}
+  EOF
+  cat <<'EOF' > main.c
+    ${mainContent}
   EOF
   direnv allow
 ''
